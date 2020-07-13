f2d766a6cbc15fc3f511f83074b00dd2191a6d50
Nhi Mai-Do <nhi_maido@outlook.com>
2020-07-13 07:34:32 -0700

    added template function Tooltip()

---
+++ src/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+//import styles from "./tooltip.module.css";
+
+function Tooltip() {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className="App">
+      <button
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>
+        Hover over me!
+      </button>
+      {isShown && (
+        <div>
+          I'll appear when you hover over the button.
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Tooltip;
